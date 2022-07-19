d6ff6bde8e25e907ebd4cee29c72bc413841eca5
josemlozano <josemlozano6@gmail.com>
2022-07-19 20:35:20 +0200

    Initial Commit

---
+++ Connect4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Connect4
+//
+//  Created by Jose Muñoz on 19/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
